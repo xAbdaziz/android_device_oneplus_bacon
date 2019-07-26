e7baa268d6eaf1da7d714503fbecdda1d97a6eef
Nicholas Lim <nicknitewolf@lineageos.org>
2019-07-26 20:46:48 +0800

    bacon: Add qcom privapp permissions
     * https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists
    
    Change-Id: Ia90d0bf3a9c2163f2bbd9202384d38f05b442e4f

--- bacon.mk
+++ bacon.mk
@@ -238,6 +238,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
