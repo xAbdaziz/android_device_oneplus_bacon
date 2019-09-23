dc7c0ed871062e1c37ac98744335c1d283c6610b
xawlw <abdulazizawlw@gmail.com>
2019-09-23 08:14:39 +0300

    bacon: Build SoundTrigger

--- bacon.mk
+++ bacon.mk
@@ -55,6 +55,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio_policy.msm8974 \
     audio.primary.msm8974 \
